3604c1ff05c1e4560c0f9ff1c7ab18748b3d4eda
Patrick Marre <pmarre40@gmail.com>
2021-06-23 07:59:48 -0700

    finished project

--- Distributed_Systems/hw1/InetServer/src/InetClient.java
+++ Distributed_Systems/hw1/InetServer/src/InetClient.java
@@ -6,25 +6,25 @@ import java.net.*;
 public class InetClient {
     public static void main(String args[]) {
         String serverName;
-        if (args.length < 1)
+        if (args.length < 1) // if command line args are less than 1, servername = localhost
             serverName = "localhost";
         else
-            serverName = args[0];
+            serverName = args[0]; // else servername is the first command line arg
 
         System.out.println("Patrick Marre's Inet Client, v1.1.\n");
         System.out.println("Using server: " + serverName + ", Port: 1565");
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in)); // create input buffer
         try {
             String name;
-            do {
+            do { // while user does not type quit do the following
                 System.out.print("Enter a host name or IP address, (quit) to end: ");
                 System.out.flush();
                 name = in.readLine();
                 if (name.indexOf("quit") < 0)
-                    getRemoteAddress(name, serverName);
+                    getRemoteAddress(name, serverName); // get the remote address of the input from the user
             } while (name.indexOf("quit") < 0);
             System.out.println("Cancelled by user request.");
-        } catch (IOException x) {
+        } catch (IOException x) { // if error print error
             x.printStackTrace();
         }
     }
@@ -45,19 +45,19 @@ public class InetClient {
         PrintStream toServer;
         String textFromServer;
         try {
-            sock = new Socket(serverName, 1565);
-            fromServer = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-            toServer = new PrintStream(sock.getOutputStream());
+            sock = new Socket(serverName, 1565); // new socket at port 1565
+            fromServer = new BufferedReader(new InputStreamReader(sock.getInputStream())); // read from the buffer input
+            toServer = new PrintStream(sock.getOutputStream()); // print to the out stream from the socket
             toServer.println(name);
             toServer.flush();
 
-            for (int i = 1; i <= 3; i++) {
+            for (int i = 1; i <= 3; i++) { // print 3 lines from the information that is returned from the server
                 textFromServer = fromServer.readLine();
                 if (textFromServer != null)
                     System.out.println(textFromServer);
             }
-            sock.close();
-        } catch (IOException x) {
+            sock.close(); // close socket to avoid leaks
+        } catch (IOException x) { // if error print error
             System.out.println("Socket error.");
             x.printStackTrace();
         }
--- Distributed_Systems/hw1/InetServer/src/InetServer.java
+++ Distributed_Systems/hw1/InetServer/src/InetServer.java
@@ -3,40 +3,44 @@ package Distributed_Systems.hw1.InetServer.src; // Add project package
 import java.io.*; // Import I/O libraries
 import java.net.*; // Import Java networking libraries
 
-class Worker extends Thread {
-    Socket sock;
+class Worker extends Thread { // Worker constructor
+    Socket sock; // init socket
 
     Worker(Socket s) {
         sock = s;
     }
 
     public void run() {
-        PrintStream out = null;
-        BufferedReader in = null;
+        PrintStream out = null; // init output stream to null
+        BufferedReader in = null; // init input to null
         try {
-            in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-            out = new PrintStream(sock.getOutputStream());
+            in = new BufferedReader(new InputStreamReader(sock.getInputStream())); // read input from inputStream of
+                                                                                   // sock (socket)
+            out = new PrintStream(sock.getOutputStream()); // printable value of sock's output stream
             try {
-                String name;
-                name = in.readLine();
+                String name; // init name
+                name = in.readLine(); // from in, read one line at a time
                 System.out.println("Looking up " + name);
-                printRemoteAddress(name, out);
-            } catch (IOException x) {
+                printRemoteAddress(name, out); // pass name and out to printRemoteAddress, prints the returned
+                                               // information to the client
+            } catch (IOException x) { // if error, print error
                 System.out.println("Server read error");
                 x.printStackTrace();
             }
-            sock.close();
-        } catch (IOException ioe) {
+            sock.close(); // close socket to avoid memory leak
+        } catch (IOException ioe) { // if error print error
             System.out.println(ioe);
         }
     }
 
     static void printRemoteAddress(String name, PrintStream out) {
+        // function is for printing the information received from the server to the the
+        // client
         try {
             out.println("Looking up " + name + "...");
-            InetAddress machine = InetAddress.getByName(name);
-            out.println("Host name: " + machine.getHostName());
-            out.println("Host IP: " + toText(machine.getAddress()));
+            InetAddress machine = InetAddress.getByName(name); // gets IP address by the name passed to function
+            out.println("Host name: " + machine.getHostName()); // gets host name by IP address
+            out.println("Host IP: " + toText(machine.getAddress())); // get IP address
         } catch (UnknownHostException ex) {
             out.println("Failed in attempt to look up " + name);
         }
@@ -56,15 +60,16 @@ class Worker extends Thread {
 public class InetServer {
     public static void main(String[] args) throws Exception {
         int q_len = 6;
-        int port = 1565;
-        Socket sock;
+        int port = 1565; // set the port
+        Socket sock; // init sock
 
-        ServerSocket servsock = new ServerSocket(port, q_len);
+        ServerSocket servsock = new ServerSocket(port, q_len); // create server socket
 
         System.out.println("Patrick Marre's Inet Server v1.1 starting up, listening at port 1565.\n");
         while (true) {
-            sock = servsock.accept();
-            new Worker(sock).start();
+            sock = servsock.accept(); // connect socket
+            new Worker(sock).start(); // created multithreads
         }
+
     }
 }
