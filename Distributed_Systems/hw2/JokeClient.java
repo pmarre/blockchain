cd604fc62bfb1c10864b96b78e78858f4540bfe3
Patrick Marre <pmarre40@gmail.com>
2021-06-28 08:16:58 -0700

    class submission

--- Distributed_Systems/hw2/JokeClient.java
+++ Distributed_Systems/hw2/JokeClient.java
@@ -1,3 +1,48 @@
+/*--------------------------------------------------------
+
+1. Patrick Marre / 6.27.21:
+
+2. Java version used (java -version), if not the official version for the class:
+
+openjdk version "11.0.11" 2021-04-20
+OpenJDK Runtime Environment AdoptOpenJDK-11.0.11+9 (build 11.0.11+9)
+OpenJDK 64-Bit Server VM AdoptOpenJDK-11.0.11+9 (build 11.0.11+9, mixed mode)
+
+3. Precise command-line compilation examples / instructions:
+
+> javac JokeClient.java
+
+
+4. Precise examples / instructions to run this program:
+
+In separate shell windows:
+
+> java JokeServer
+> java JokeClient
+> java JokeClientAdmin
+
+All acceptable commands are displayed on the various consoles.
+
+This may run across multiple machines but has had some issues. To test it, try the following with the IP Address as the second argument.
+
+e.g. if you IP Address is 140.192.1.22 then you would type:
+
+> java JokeClient 140.192.1.22
+> java JokeClientAdmin 140.192.1.22
+
+5. Files needed for running the program.
+
+ 1. checklist.html
+ 2. JokeServer.java
+ 3. JokeClient.java
+ 4. JokeClientAdmin.java
+
+5. Notes:
+
+When not using 'localhost', it has been buggy and timing out. 
+
+----------------------------------------------------------*/
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,56 +51,45 @@ import java.net.Socket;
 import java.util.*; // For UUID 
 
 public class JokeClient {
+    static final UUID uuid = UUID.randomUUID();
+    static String name;
+
     public static void main(String args[]) {
+
         String serverName;
-        UUID uuid = UUID.randomUUID();
         if (args.length < 1) // if command line args are less than 1, servername = localhost
+
             serverName = "localhost";
         else
             serverName = args[0]; // else servername is the first command line arg
 
         System.out.println("Patrick Marre's Joke Client, v1.1.\n");
-        System.out.println("Using server: " + serverName + ", Port: 1565");
+        System.out.println("Using server: " + serverName + ", Port: 4545");
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in)); // create input buffer
         try {
-            String name;
             String mode;
-            System.out.print("Enter your username, (quit) to end: ");
+            System.out.print("Enter your username or type 'quit' to end: ");
             System.out.flush();
-            name = in.readLine();
-            int j_count = 0;
-            int p_count = 0;
-            do { // while user does not type quit do the following
-
-                System.out.print("Would you like a joke or proverb?, (quit) to end: ");
+            name = in.readLine(); // get username
+            do {
+                System.out.print("Press enter for joke or proverb, (y\\n): "); // ask to see another joke/proverb
                 System.out.flush();
                 mode = in.readLine();
-                if (name.indexOf("quit") < 0) {
-                    if (mode.indexOf("quit") < 0) {
-                        if (mode.equals("joke")) {
-                            getRemoteAddress(uuid, name, mode, serverName, j_count); // get the remote address of the input
-                                                                               // from the
-                                                                               // user
-                            if (j_count == 3)
-                                j_count = 0;
-                            else
-                                j_count++;
-                        } else if (mode.equals("proverb")) {
-                            getRemoteAddress(uuid, name, mode, serverName, p_count); // get the remote address of the input
-                                                                               // from the
-                            // user
-                            if (p_count == 3)
-                                p_count = 0;
-                            else
-                                p_count++;
-                        }
+                if (name.indexOf("n") < 0) {
+                    if (mode.equals("y")) {
+                        // send joke/proverb over if yes
+                        sendState(uuid, name, mode, serverName);
                     }
+
                 }
-            } while (name.indexOf("quit") < 0 || mode.indexOf("quit") < 0);
-            System.out.println("Cancelled by user request.");
-        } catch (IOException x) { // if error print error
+            } while ((mode.equals("y")));
+
+        } catch (
+
+        IOException x) {
             x.printStackTrace();
         }
+
     }
 
     static String toText(byte ip[]) {
@@ -68,26 +102,27 @@ public class JokeClient {
         return result.toString();
     }
 
-    static void getRemoteAddress(UUID uuid, String name, String mode, String serverName, int count) {
+    static void sendState(UUID uuid, String name, String mode, String serverName) {
+        // This a state management function that communicates with the server to pass infomation such as name, uuide, server, etc.
         Socket sock;
-        BufferedReader fromServer;
-        PrintStream toServer;
-        String textFromServer;
+        BufferedReader from_server;
+        PrintStream to_server;
+        String txt_from_server;
         try {
-            sock = new Socket(serverName, 1565); // new socket at port 1565
-            fromServer = new BufferedReader(new InputStreamReader(sock.getInputStream())); // read from the buffer input
-            toServer = new PrintStream(sock.getOutputStream()); // print to the out stream from the socket
-            toServer.println(uuid);
-            toServer.println(name);
-            toServer.println(count);
-            toServer.println(mode);
+            sock = new Socket(serverName, 4545); // new socket at port 4545
+            from_server = new BufferedReader(new InputStreamReader(sock.getInputStream())); // read from the buffer
+                                                                                            // input
+            to_server = new PrintStream(sock.getOutputStream()); // print to the out stream from the socket
+            to_server.println(uuid); // send server uuid
+            to_server.println(name); // send server name
+            to_server.println(mode); // send server mode
 
-            toServer.flush();
+            to_server.flush();
 
             for (int i = 1; i <= 3; i++) { // print 3 lines from the information that is returned from the server
-                textFromServer = fromServer.readLine();
-                if (textFromServer != null)
-                    System.out.println(textFromServer);
+                txt_from_server = from_server.readLine();
+                if (txt_from_server != null)
+                    System.out.println(txt_from_server);
             }
             sock.close(); // close socket to avoid leaks
         } catch (IOException x) { // if error print error
--- Distributed_Systems/hw2/JokeClientAdmin.java
+++ Distributed_Systems/hw2/JokeClientAdmin.java
@@ -1,3 +1,47 @@
+/*--------------------------------------------------------
+1. Patrick Marre / 6.27.21:
+
+2. Java version used (java -version), if not the official version for the class:
+
+openjdk version "11.0.11" 2021-04-20
+OpenJDK Runtime Environment AdoptOpenJDK-11.0.11+9 (build 11.0.11+9)
+OpenJDK 64-Bit Server VM AdoptOpenJDK-11.0.11+9 (build 11.0.11+9, mixed mode)
+
+3. Precise command-line compilation examples / instructions:
+
+> javac JokeClientAdmin.java
+
+
+4. Precise examples / instructions to run this program:
+
+In separate shell windows:
+
+> java JokeServer
+> java JokeClient
+> java JokeClientAdmin
+
+All acceptable commands are displayed on the various consoles.
+
+This may run across multiple machines but has had some issues. To test it, try the following with the IP Address as the second argument.
+
+e.g. if you IP Address is 140.192.1.22 then you would type:
+
+> java JokeClient 140.192.1.22
+> java JokeClientAdmin 140.192.1.22
+
+5. Files needed for running the program.
+
+ 1. checklist.html
+ 2. JokeServer.java
+ 3. JokeClient.java
+ 4. JokeClientAdmin.java
+
+5. Notes:
+
+When not using 'localhost', it has been buggy and timing out. 
+
+----------------------------------------------------------*/
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,50 +52,27 @@ public class JokeClientAdmin {
 
     public static void main(String args[]) {
         String serverName;
-        String current_mode = "joke";
         if (args.length < 1) // if command line args are less than 1, servername = localhost
             serverName = "localhost";
         else
             serverName = args[0]; // else servername is the first command line arg
-
+        System.out.println("Patrick Marre's Joke Client, v1.1.\n");
+        System.out.println("Using server: " + serverName + ", Port: 5050");
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in)); // create input buffer
 
         try {
-            JokeClientAdmin jokes = new JokeClientAdmin();
-            String name;
             String mode;
-            System.out.print("Enter your username, (quit) to end: ");
-            System.out.flush();
-            name = in.readLine();
-            int j_count = 0;
-            int p_count = 0;
             do { // while user does not type quit do the following
-
-                System.out.print("Would you like a joke or proverb?, (quit) to end: ");
+                System.out.print("Type 'joke' for joke mode or 'proverb' for proverb mode: ");
                 System.out.flush();
-                mode = in.readLine();
-                if (name.indexOf("quit") < 0) {
-                    if (mode.indexOf("quit") < 0) {
-                        if (mode.equals("joke")) {
-                            setState(name, mode, serverName, j_count); // get the remote address of the input
-                                                                       // from the
-                                                                       // user
-                            if (j_count == 3)
-                                j_count = 0;
-                            else
-                                j_count++;
-                        } else if (mode.equals("proverb")) {
-                            setState(name, mode, serverName, p_count); // get the remote address of the input
-                                                                       // from the
-                            // user
-                            if (p_count == 3)
-                                p_count = 0;
-                            else
-                                p_count++;
-                        }
+                mode = in.readLine(); // get new mode from user
+                if (mode.indexOf("quit") < 0) {
+                    if (mode.equals("joke") || mode.equals("proverb")) {
+                        setMode(mode, serverName); // set the mode
+                        System.out.println("Changed mode to " + mode);
                     }
                 }
-            } while (name.indexOf("quit") < 0 || mode.indexOf("quit") < 0);
+            } while (((mode.equals("joke")) || (mode.equals("proverb"))));
             System.out.println("Cancelled by user request.");
         } catch (IOException x) { // if error print error
             x.printStackTrace();
@@ -68,25 +89,24 @@ public class JokeClientAdmin {
         return result.toString();
     }
 
-    static void setState(String name, String mode, String serverName, int count) {
+    static void setMode(String mode, String serverName) {
         Socket sock;
-        BufferedReader fromServer;
-        PrintStream toServer;
-        String textFromServer;
-        try {
-            sock = new Socket(serverName, 1565); // new socket at port 1565
-            fromServer = new BufferedReader(new InputStreamReader(sock.getInputStream())); // read from the buffer input
-            toServer = new PrintStream(sock.getOutputStream()); // print to the out stream from the socket
-            toServer.println(name);
-            toServer.println(count);
-            toServer.println(mode);
+        BufferedReader from_server;
+        PrintStream to_server;
+        String txt_from_server;
 
-            toServer.flush();
+        try {
+            sock = new Socket(serverName, 5050); // new socket at port 5050
+            from_server = new BufferedReader(new InputStreamReader(sock.getInputStream())); // read from the buffer
+                                                                                            // input
+            to_server = new PrintStream(sock.getOutputStream()); // print to the out stream from the socket
+            to_server.println(mode); // send mode to the server
+            to_server.flush();
 
             for (int i = 1; i <= 3; i++) { // print 3 lines from the information that is returned from the server
-                textFromServer = fromServer.readLine();
-                if (textFromServer != null)
-                    System.out.println(textFromServer);
+                txt_from_server = from_server.readLine();
+                if (txt_from_server != null)
+                    System.out.println(txt_from_server);
             }
             sock.close(); // close socket to avoid leaks
         } catch (IOException x) { // if error print error
